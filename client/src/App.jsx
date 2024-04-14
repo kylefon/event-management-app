f3b854bdf273926338c11960534dada7c619e572
unknown <robnekyle@gmail.com>
2024-04-14 19:11:18 +0800

    Add dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import InputCustomer from './components/inputCustomer'
 import ListCustomers from './components/listCustomer'
+import InputOrder from './components/inputOrder'
+// import ListOrder from './components/listOrder'
 
 function App() {
     return (
-        <>
-            <InputCustomer />
-            <ListCustomers />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={
+                    <>
+                        <InputCustomer />
+                        <ListCustomers />
+                    </>
+                }>  
+                </Route>
+                <Route exact path='/order' element={<InputOrder />}></Route> 
+            </Routes>
+        </BrowserRouter>
+            
     )
 };
 export default App
